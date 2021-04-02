d2414357bb925cdaeaef59585f4a3709046624da
19Michael91 <vmv.asap@gmail.com>
2021-04-02 09:34:55 +0300

    added name to route

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', 'Api\UsersController@index');
+Route::get('/users', 'Api\UsersController@index')->name('api.users.index');
